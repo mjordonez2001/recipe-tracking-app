301ba61233bd0b146f22b319eda2f2e13d58a206
Maria Ordonez <ordonezyuyis@gmail.com>
2022-02-20 18:56:12 -0600

    Updates

---
+++ precache-manifest.a0a2cef7142942e1b39b9fb6fcf44544.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f4e4ece1881e5dd659ffc67a7d9146",
+    "url": "/recipe-tracking-app/index.html"
+  },
+  {
+    "revision": "c8d438291cdcc57b5452",
+    "url": "/recipe-tracking-app/static/css/main.d7b96a50.chunk.css"
+  },
+  {
+    "revision": "f394498dd0083a989c2b",
+    "url": "/recipe-tracking-app/static/js/2.6d741cec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe-tracking-app/static/js/2.6d741cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d438291cdcc57b5452",
+    "url": "/recipe-tracking-app/static/js/main.adf04bb9.chunk.js"
+  },
+  {
+    "revision": "4cba0eb1be850e6e3ce8",
+    "url": "/recipe-tracking-app/static/js/runtime-main.68250650.js"
+  }
+]);
